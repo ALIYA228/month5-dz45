90f527041542fbd9f4fe970c5ed328fd168c2f2f
ALIYA228 <aabilova2007@gmail.com>
2024-09-22 13:56:17 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect} from 'react';
+import {useDispatch, useSelector} from "react-redux";
+import {getAsyncData} from "./reducer/getPotterReducer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const dispatch = useDispatch()
+    const {persons} = useSelector(state => state.getPotterReducer)
+    useEffect(() => {
+        dispatch(getAsyncData())
+    }, [])
+    console.log(persons)
+    return (
+        <div>
+            {
+                persons?(
+                    persons.map((item)=> (
+                        <div>
+                            <span>{item.userId}</span>
+                        </div>
+                    ))
+                ):(
+                    <span>Нету данных родной</span>
+                )
+            }
+        </div>
+    );
+};
 
 export default App;
